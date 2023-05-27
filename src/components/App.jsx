0706b3f29a218d2c74b235ff1199132b933e8c41
MateuszCharysz <charyszek@interia.pl>
2023-05-27 19:03:04 +0200

    first part ready
    not looking nice but working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,92 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackButton from 'components/feedback-button/feedback-button';
+import { nanoid } from 'nanoid';
+import FeedbackItemRender from 'components/feedback-item-render/feedback-item-render';
+import FuncItemRender from 'components/total-count/func-item-render';
+import Section from './section/section';
+import Notification from './notification/notification';
+
+export class App extends Component {
+  static propTypes = { good: Number, neutral: Number, bad: Number };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  goodBtnHandle = () => {
+    this.setState({
+      good: this.state.good + 1,
+    });
+  };
+
+  neutralBtnHandle = () => {
+    this.setState({
+      neutral: this.state.neutral + 1,
+    });
+  };
+
+  badBtnHandle = () => {
+    this.setState({
+      bad: this.state.bad + 1,
+    });
+  };
+
+  countTotalFeedback = () =>
+    this.state.bad + this.state.neutral + this.state.good;
+
+  countPositiveFeedbackPercentage = () =>
+    ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
+
+  render() {
+    return (
+      <>
+        <Section
+          title="Please leave feedback"
+          render={Object.keys(this.state).map(feedback => (
+            <FeedbackButton
+              name={feedback}
+              key={nanoid()}
+              func={eval(`this.${feedback}BtnHandle`)}
+            />
+          ))}
+        />
+
+        {JSON.stringify(this.state) ===
+        JSON.stringify({ good: 0, neutral: 0, bad: 0 }) ? (
+          <Section
+            title="Statistics"
+            reactComp={<Notification messege="There is no feedback" />}
+          />
+        ) : (
+          <Section
+            title="Statistics"
+            render={Object.entries(this.state).map(value => (
+              <FeedbackItemRender
+                objkey={value[0]}
+                value={value[1]}
+                key={nanoid()}
+              />
+            ))}
+            reactComp={
+              <div>
+                <FuncItemRender
+                  title="total"
+                  func={this.countTotalFeedback()}
+                  key={nanoid()}
+                />
+                <FuncItemRender
+                  title="Positive feedback"
+                  func={this.countPositiveFeedbackPercentage()}
+                  percent="%"
+                  key={nanoid()}
+                />
+              </div>
+            }
+          />
+        )}
+      </>
+    );
+  }
+}
